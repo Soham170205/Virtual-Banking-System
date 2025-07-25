26834984e99ace6e39d93da12e88bffd579780d2
Soham170205 <shimpisoham44@gmail.com>
2025-07-25 10:07:10 +0530

    Update Adashboard.java

--- Virtual-Banking-System/src/Adashboard.java
+++ Virtual-Banking-System/src/Adashboard.java
@@ -9,7 +9,7 @@ class Adashboard extends JFrame {
         Font tableFont = new Font("Calibri", Font.PLAIN, 18);
         Font buttonFont = new Font("Calibri", Font.BOLD, 20);
 
-        //Already likha hua aata hai
+    
         JLabel title = new JLabel("Admin Dashboard", JLabel.CENTER);
         title.setFont(titleFont);
         title.setForeground(Color.WHITE);
@@ -35,7 +35,7 @@ class Adashboard extends JFrame {
 
         JScrollPane scrollPane = new JScrollPane(table);
 
-        // jismai user likhta hai
+      
         JTextField t1 = new JTextField(10);
         JTextField t2 = new JTextField(10);
 
@@ -79,7 +79,7 @@ class Adashboard extends JFrame {
         centerPanel.add(filterPanel, BorderLayout.NORTH);
         centerPanel.add(scrollPane, BorderLayout.CENTER);
 
-        //container to activate the labels and stuff
+     
         Container c = getContentPane();
         c.setLayout(new BorderLayout(20, 20));
         c.add(topPanel, BorderLayout.NORTH);
@@ -89,9 +89,9 @@ class Adashboard extends JFrame {
         b1.addActionListener(
                 a->
                 {
-                    //0th row se shuru hoga
+                 
                     tableModel.setRowCount(0);
-                    //get min and max balance
+               
                     String str1 = t1.getText();
                     String str2 = t2.getText();
                     double min,max;
@@ -114,23 +114,23 @@ class Adashboard extends JFrame {
                     }
 
                     String url = "jdbc:mysql://localhost:3306/batch2";
-                    try (Connection con = DriverManager.getConnection(url, "root", "SohamSQL#1211")) {
-                        //users between minimum and maximum will be displayed
+                    try (Connection con = DriverManager.getConnection(url, "root", "")) {
+                        
                         String sql = "select * from users where balance between "+min+" and "+max;
                         try (PreparedStatement pst = con.prepareStatement(sql))
                         {
-                            //result set create karna hai for all users
+                            
                             ResultSet rs = pst.executeQuery();
                             while (rs.next())
                             {
-                                //info to be displayed
+                               
                                 String s1 = rs.getString("username");
                                 double d1 = rs.getDouble("balance");
                                 String s2 = rs.getString("phone");
                                 String s3 = rs.getString("email");
                                 String s4 = rs.getString("gender");
                                 double d2 = rs.getDouble("wlimit");
-                                //it will display all of the above things in table format columnwise
+                               
                                 tableModel.addRow(new Object[]{s1,d1,s2,s3,s4,d2});
                             }
                         }
@@ -141,7 +141,7 @@ class Adashboard extends JFrame {
                 }
         );
 
-        //this entire try catch exists to display all users by default without the min and max values
+       
         String url = "jdbc:mysql://localhost:3306/batch2";
         try (Connection con = DriverManager.getConnection(url, "root", "SohamSQL#1211")) {
             String sql = "select * from users";
@@ -166,15 +166,15 @@ class Adashboard extends JFrame {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
 
-        //title set karega upar 
+    
         setTitle("Admin Dashboard");
-        //increases size of things to br visible on screen
+      
         setSize(900, 600);
-        //sab contents ko middle mai laneke liye
+  
         setLocationRelativeTo(null);
-        //this will exit the code on closing the GUI
+       
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        //will make all the things visible (makes frame visible on screen)
+       
         setVisible(true);
     }
 
@@ -184,24 +184,6 @@ class Adashboard extends JFrame {
     }
 }
 
-//cell editable and swapable
-
-
-/*
-1) delete account option
-    users
-    transactions
-
-2) background images
-3) Change wlimit (alag if else)
-4) KYC
-5) Types of accounts upon age
-6) BAnk account login ke liye OTp through email
-7) Password encryption
-8) Login pe ek forgot password and mail pe otp to reset password (requiress internet)
-9) Display All Schemes , loans ,investment opportunities
-10) think about it later
 
 
 
- */
